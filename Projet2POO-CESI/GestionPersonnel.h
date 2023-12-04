5b13c7a610469edab102680b7b03d37f8deac672
4Sitam4 <109106334+4Sitam4@users.noreply.github.com>
2023-12-04 22:34:57 +0100

    Ajout TextBox Supérieur pour ajout membre personnel

--- Projet2POO-CESI/GestionPersonnel.h
+++ Projet2POO-CESI/GestionPersonnel.h
@@ -48,6 +48,10 @@ namespace Projet2POOCESI {
 	private: System::Windows::Forms::Label^ labelPersoAdresse;
 	private: System::Windows::Forms::TextBox^ textBoxPersoAdresse;
 	private: System::Windows::Forms::Button^ buttonEmbaucher;
+	private: System::Windows::Forms::Label^ labelSuperieur;
+	private: System::Windows::Forms::TextBox^ textBoxSuperieur;
+
+
 
 
 
@@ -67,22 +71,26 @@ namespace Projet2POOCESI {
 		void InitializeComponent(void)
 		{
 			this->groupBox1 = (gcnew System::Windows::Forms::GroupBox());
-			this->backgroundWorker1 = (gcnew System::ComponentModel::BackgroundWorker());
-			this->textBoxPersoNom = (gcnew System::Windows::Forms::TextBox());
-			this->labelPersoNom = (gcnew System::Windows::Forms::Label());
-			this->labelPersoPrenom = (gcnew System::Windows::Forms::Label());
-			this->textBoxPersoPrenom = (gcnew System::Windows::Forms::TextBox());
+			this->buttonEmbaucher = (gcnew System::Windows::Forms::Button());
+			this->labelDateEmbauche = (gcnew System::Windows::Forms::Label());
+			this->dateTimePickerDateEmbauche = (gcnew System::Windows::Forms::DateTimePicker());
 			this->labelPersoAdresse = (gcnew System::Windows::Forms::Label());
 			this->textBoxPersoAdresse = (gcnew System::Windows::Forms::TextBox());
-			this->dateTimePickerDateEmbauche = (gcnew System::Windows::Forms::DateTimePicker());
-			this->labelDateEmbauche = (gcnew System::Windows::Forms::Label());
-			this->buttonEmbaucher = (gcnew System::Windows::Forms::Button());
+			this->labelPersoPrenom = (gcnew System::Windows::Forms::Label());
+			this->textBoxPersoPrenom = (gcnew System::Windows::Forms::TextBox());
+			this->labelPersoNom = (gcnew System::Windows::Forms::Label());
+			this->textBoxPersoNom = (gcnew System::Windows::Forms::TextBox());
+			this->backgroundWorker1 = (gcnew System::ComponentModel::BackgroundWorker());
+			this->labelSuperieur = (gcnew System::Windows::Forms::Label());
+			this->textBoxSuperieur = (gcnew System::Windows::Forms::TextBox());
 			this->groupBox1->SuspendLayout();
 			this->SuspendLayout();
 			// 
 			// groupBox1
 			// 
 			this->groupBox1->BackColor = System::Drawing::SystemColors::ControlLightLight;
+			this->groupBox1->Controls->Add(this->labelSuperieur);
+			this->groupBox1->Controls->Add(this->textBoxSuperieur);
 			this->groupBox1->Controls->Add(this->buttonEmbaucher);
 			this->groupBox1->Controls->Add(this->labelDateEmbauche);
 			this->groupBox1->Controls->Add(this->dateTimePickerDateEmbauche);
@@ -94,28 +102,55 @@ namespace Projet2POOCESI {
 			this->groupBox1->Controls->Add(this->textBoxPersoNom);
 			this->groupBox1->Location = System::Drawing::Point(12, 12);
 			this->groupBox1->Name = L"groupBox1";
-			this->groupBox1->Size = System::Drawing::Size(234, 194);
+			this->groupBox1->Size = System::Drawing::Size(234, 224);
 			this->groupBox1->TabIndex = 0;
 			this->groupBox1->TabStop = false;
 			this->groupBox1->Text = L"Ajout membre du personnel";
 			this->groupBox1->Enter += gcnew System::EventHandler(this, &GestionPersonnel::groupBox1_Enter);
 			// 
-			// textBoxPersoNom
+			// buttonEmbaucher
 			// 
-			this->textBoxPersoNom->Location = System::Drawing::Point(6, 34);
-			this->textBoxPersoNom->Name = L"textBoxPersoNom";
-			this->textBoxPersoNom->Size = System::Drawing::Size(100, 20);
-			this->textBoxPersoNom->TabIndex = 0;
+			this->buttonEmbaucher->Location = System::Drawing::Point(74, 195);
+			this->buttonEmbaucher->Name = L"buttonEmbaucher";
+			this->buttonEmbaucher->Size = System::Drawing::Size(75, 23);
+			this->buttonEmbaucher->TabIndex = 8;
+			this->buttonEmbaucher->Text = L"Embaucher";
+			this->buttonEmbaucher->UseVisualStyleBackColor = true;
 			// 
-			// labelPersoNom
+			// labelDateEmbauche
 			// 
-			this->labelPersoNom->AutoSize = true;
-			this->labelPersoNom->Location = System::Drawing::Point(3, 18);
-			this->labelPersoNom->Name = L"labelPersoNom";
-			this->labelPersoNom->Size = System::Drawing::Size(29, 13);
-			this->labelPersoNom->TabIndex = 1;
-			this->labelPersoNom->Text = L"Nom";
-			this->labelPersoNom->Click += gcnew System::EventHandler(this, &GestionPersonnel::label1_Click);
+			this->labelDateEmbauche->AutoSize = true;
+			this->labelDateEmbauche->Location = System::Drawing::Point(6, 147);
+			this->labelDateEmbauche->Name = L"labelDateEmbauche";
+			this->labelDateEmbauche->Size = System::Drawing::Size(91, 13);
+			this->labelDateEmbauche->TabIndex = 7;
+			this->labelDateEmbauche->Text = L"Date d\'embauche";
+			this->labelDateEmbauche->Click += gcnew System::EventHandler(this, &GestionPersonnel::label1_Click_2);
+			// 
+			// dateTimePickerDateEmbauche
+			// 
+			this->dateTimePickerDateEmbauche->Location = System::Drawing::Point(6, 163);
+			this->dateTimePickerDateEmbauche->MaxDate = System::DateTime(2999, 12, 31, 0, 0, 0, 0);
+			this->dateTimePickerDateEmbauche->MinDate = System::DateTime(1900, 1, 1, 0, 0, 0, 0);
+			this->dateTimePickerDateEmbauche->Name = L"dateTimePickerDateEmbauche";
+			this->dateTimePickerDateEmbauche->Size = System::Drawing::Size(219, 20);
+			this->dateTimePickerDateEmbauche->TabIndex = 6;
+			// 
+			// labelPersoAdresse
+			// 
+			this->labelPersoAdresse->AutoSize = true;
+			this->labelPersoAdresse->Location = System::Drawing::Point(3, 59);
+			this->labelPersoAdresse->Name = L"labelPersoAdresse";
+			this->labelPersoAdresse->Size = System::Drawing::Size(45, 13);
+			this->labelPersoAdresse->TabIndex = 5;
+			this->labelPersoAdresse->Text = L"Adresse";
+			// 
+			// textBoxPersoAdresse
+			// 
+			this->textBoxPersoAdresse->Location = System::Drawing::Point(6, 75);
+			this->textBoxPersoAdresse->Name = L"textBoxPersoAdresse";
+			this->textBoxPersoAdresse->Size = System::Drawing::Size(219, 20);
+			this->textBoxPersoAdresse->TabIndex = 4;
 			// 
 			// labelPersoPrenom
 			// 
@@ -135,49 +170,39 @@ namespace Projet2POOCESI {
 			this->textBoxPersoPrenom->TabIndex = 2;
 			this->textBoxPersoPrenom->TextChanged += gcnew System::EventHandler(this, &GestionPersonnel::textBox1_TextChanged);
 			// 
-			// labelPersoAdresse
+			// labelPersoNom
 			// 
-			this->labelPersoAdresse->AutoSize = true;
-			this->labelPersoAdresse->Location = System::Drawing::Point(3, 68);
-			this->labelPersoAdresse->Name = L"labelPersoAdresse";
-			this->labelPersoAdresse->Size = System::Drawing::Size(45, 13);
-			this->labelPersoAdresse->TabIndex = 5;
-			this->labelPersoAdresse->Text = L"Adresse";
+			this->labelPersoNom->AutoSize = true;
+			this->labelPersoNom->Location = System::Drawing::Point(3, 18);
+			this->labelPersoNom->Name = L"labelPersoNom";
+			this->labelPersoNom->Size = System::Drawing::Size(29, 13);
+			this->labelPersoNom->TabIndex = 1;
+			this->labelPersoNom->Text = L"Nom";
+			this->labelPersoNom->Click += gcnew System::EventHandler(this, &GestionPersonnel::label1_Click);
 			// 
-			// textBoxPersoAdresse
+			// textBoxPersoNom
 			// 
-			this->textBoxPersoAdresse->Location = System::Drawing::Point(6, 84);
-			this->textBoxPersoAdresse->Name = L"textBoxPersoAdresse";
-			this->textBoxPersoAdresse->Size = System::Drawing::Size(100, 20);
-			this->textBoxPersoAdresse->TabIndex = 4;
+			this->textBoxPersoNom->Location = System::Drawing::Point(6, 34);
+			this->textBoxPersoNom->Name = L"textBoxPersoNom";
+			this->textBoxPersoNom->Size = System::Drawing::Size(100, 20);
+			this->textBoxPersoNom->TabIndex = 0;
 			// 
-			// dateTimePickerDateEmbauche
+			// labelSuperieur
 			// 
-			this->dateTimePickerDateEmbauche->Location = System::Drawing::Point(6, 134);
-			this->dateTimePickerDateEmbauche->MaxDate = System::DateTime(2999, 12, 31, 0, 0, 0, 0);
-			this->dateTimePickerDateEmbauche->MinDate = System::DateTime(1900, 1, 1, 0, 0, 0, 0);
-			this->dateTimePickerDateEmbauche->Name = L"dateTimePickerDateEmbauche";
-			this->dateTimePickerDateEmbauche->Size = System::Drawing::Size(219, 20);
-			this->dateTimePickerDateEmbauche->TabIndex = 6;
+			this->labelSuperieur->AutoSize = true;
+			this->labelSuperieur->Location = System::Drawing::Point(6, 99);
+			this->labelSuperieur->Name = L"labelSuperieur";
+			this->labelSuperieur->Size = System::Drawing::Size(52, 13);
+			this->labelSuperieur->TabIndex = 10;
+			this->labelSuperieur->Text = L"Supérieur";
+			this->labelSuperieur->Click += gcnew System::EventHandler(this, &GestionPersonnel::label1_Click_3);
 			// 
-			// labelDateEmbauche
+			// textBoxSuperieur
 			// 
-			this->labelDateEmbauche->AutoSize = true;
-			this->labelDateEmbauche->Location = System::Drawing::Point(6, 118);
-			this->labelDateEmbauche->Name = L"labelDateEmbauche";
-			this->labelDateEmbauche->Size = System::Drawing::Size(91, 13);
-			this->labelDateEmbauche->TabIndex = 7;
-			this->labelDateEmbauche->Text = L"Date d\'embauche";
-			this->labelDateEmbauche->Click += gcnew System::EventHandler(this, &GestionPersonnel::label1_Click_2);
-			// 
-			// buttonEmbaucher
-			// 
-			this->buttonEmbaucher->Location = System::Drawing::Point(80, 165);
-			this->buttonEmbaucher->Name = L"buttonEmbaucher";
-			this->buttonEmbaucher->Size = System::Drawing::Size(75, 23);
-			this->buttonEmbaucher->TabIndex = 8;
-			this->buttonEmbaucher->Text = L"Embaucher";
-			this->buttonEmbaucher->UseVisualStyleBackColor = true;
+			this->textBoxSuperieur->Location = System::Drawing::Point(6, 115);
+			this->textBoxSuperieur->Name = L"textBoxSuperieur";
+			this->textBoxSuperieur->Size = System::Drawing::Size(219, 20);
+			this->textBoxSuperieur->TabIndex = 9;
 			// 
 			// GestionPersonnel
 			// 
@@ -202,6 +227,8 @@ namespace Projet2POOCESI {
 private: System::Void textBox1_TextChanged(System::Object^ sender, System::EventArgs^ e) {
 	}
 private: System::Void label1_Click_2(System::Object^ sender, System::EventArgs^ e) {
+}
+private: System::Void label1_Click_3(System::Object^ sender, System::EventArgs^ e) {
 }
 };
 }
