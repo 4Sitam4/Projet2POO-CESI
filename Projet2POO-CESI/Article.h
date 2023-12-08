6c24bcc0d50f0d174a068c466407b5b878c11962
Mathis B <109106334+4Sitam4@users.noreply.github.com>
2023-12-08 09:39:31 +0100

    Article.h et .cpp

--- Projet2POO-CESI/Article.h
+++ Projet2POO-CESI/Article.h
@@ -1,5 +1,34 @@
 #pragma once
+
+namespace NS_Comp_Article{
+
 ref class Article
 {
+	private:
+	System::String^ Reference;
+	System::String^ Nom;
+	double Prix;
+	int Quantite;
+
+	public:
+		Article();
+		Article(System::String^ ref, System::String^ nom, double prix, int quantite);
+
+		System::String^ get_ref();
+		System::String^ get_nom();
+		double get_prix();
+		int get_quantite();
+
+		void set_ref(System::String^ ref);
+		void set_nom(System::String^ nom);
+		void set_prix(double prix);
+		void set_quantite(int quantite);
+
+		// Quantite * Prix = Prix total
+		double sommeQxP(int quantite, double prix);
+
+		~Article();
+
 };
+}
 
